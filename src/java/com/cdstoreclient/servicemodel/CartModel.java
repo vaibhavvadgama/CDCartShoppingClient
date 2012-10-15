7cf1b22e00a08ce6eeb6af739acf672b3812ff32
Vaibhav Vadgama <vaibhav.vadgama.it@gmail.com>
2012-10-15 16:31:19 -0400

    Function for Cart Model

--- src/java/com/cdstoreclient/servicemodel/CartModel.java
+++ src/java/com/cdstoreclient/servicemodel/CartModel.java
@@ -4,9 +4,13 @@
  */
 package com.cdstoreclient.servicemodel;
 
+import com.cdstoreclient.exception.CDCartException;
+import com.cdstoreserver.ws.orderprocess.AddressBean;
+import com.cdstoreserver.ws.orderprocess.CdBean;
 import com.cdstoreserver.ws.orderprocess.OrderProcessWS;
 import com.cdstoreserver.ws.orderprocess.OrderProcessWS_Service;
-
+import com.cdstoreserver.ws.orderprocess.ProcessedOrdersBean;
+import java.util.ArrayList;
 
 /**
  *
@@ -16,31 +20,47 @@ public class CartModel {
 
     OrderProcessWS_Service serviceReference = null;
     OrderProcessWS webServicePort = null;
-    
+
     public CartModel() {
-        if(serviceReference == null) {
+        if (serviceReference == null) {
             serviceReference = new OrderProcessWS_Service();
             webServicePort = serviceReference.getOrderProcessWSPort();
         }
     }
-    
+
     public OrderProcessWS getServicePort() {
-        if(serviceReference == null) {
+        if (serviceReference == null) {
             serviceReference = new OrderProcessWS_Service();
             webServicePort = serviceReference.getOrderProcessWSPort();
         }
         return webServicePort;
     }
-    
-//    public CategoryList getCategoryList() {
-//        CategoryList list = null;
-//        
-//        ProductCatalogWS_Service serviceReference = new ProductCatalogWS_Service();
-//        ProductCatalogWS webServicePort = serviceReference.getProductCatalogWSPort();
-//        
-//        list = webServicePort.getCategoryList();
-//        
-//        return list;
-//    }
-    
+
+    public ProcessedOrdersBean createOrder(ArrayList<CdBean> orderItems, AddressBean shippingInfo) throws CDCartException {
+        ProcessedOrdersBean serviceResponseObject = webServicePort.createOrder(orderItems, shippingInfo);
+
+        if (serviceResponseObject == null) {
+            throw new CDCartException(404, "Web service unavailable.");
+        } else {
+            if (!serviceResponseObject.getStatus().equals("success")) {
+                throw new CDCartException(404, serviceResponseObject.getErrormessage());
+            } else {
+                return serviceResponseObject;
+            }
+        }
+    }
+
+    public ProcessedOrdersBean confirmOrder(ProcessedOrdersBean purchasedOrder, Boolean paymentInfo) throws CDCartException {
+        ProcessedOrdersBean serviceResponseObject = webServicePort.confirmOrder(purchasedOrder, paymentInfo);
+
+        if (serviceResponseObject == null) {
+            throw new CDCartException(404, "Web service unavailable.");
+        } else {
+            if (!serviceResponseObject.getStatus().equals("success")) {
+                throw new CDCartException(404, serviceResponseObject.getErrormessage());
+            } else {
+                return serviceResponseObject;
+            }
+        }
+    }
 }
